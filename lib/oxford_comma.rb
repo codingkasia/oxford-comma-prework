690ef8235f5b50652adb0301f50006af4c883f5a
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-03 17:39:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(arr)
   if arr.size <= 1  
     arr.join()
   elsif((arr.size >1) && (arr.size < 3))
-    arr.join("and ")
+    arr.join(" and ")
   else
     last = arr.pop
     arr.join(", ") << ", and "<< last
